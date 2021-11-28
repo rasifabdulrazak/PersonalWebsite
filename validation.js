6b2cab18cf111d57a5c26ef811ee5bfd0e19231f
Rasif Abdul Razak <90362463+rasifabdulrazak@users.noreply.github.com>
2021-11-29 00:19:59 +0530

    Update validation.js

--- validation.js
+++ validation.js
@@ -7,7 +7,35 @@ var submit = document.getElementById('submit-form')
 var charecters = /^[a-zA-Z]+$/
 var mailcode =  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 var phonenumber = /^((\+[1-9]{1,4}[ \-])|(\([0-9]{2,3}\)[ \-])|([0-9]{2,4})[ \-])?[0-9]{3,4}?[ \-]*[0-9]{3,4}?$/;
+var def=0
 
+$("#submit-form").submit((e)=>{
+
+    def=0;
+     e.preventDefault()
+     validation()
+     emailpart()
+     mobilepart()
+     messagepart()
+     if(def==4){
+
+
+     $.ajax({
+         url:"https://script.google.com/macros/s/AKfycbwV3CZni70BnOezJ4c7htuXiuLPUMijejJZ55awpGV5iZdwr1WffJmr3MCnL3o1gNxS9A/exec",
+         data:$("#submit-form").serialize(),
+         method:"post",
+         success:function (response){
+             alert("Form submitted successfully")
+             window.location.reload()
+             //window.location.href="https://google.com"
+         },
+         error:function (err){
+             alert("Something Error")
+
+         }
+     })
+   }
+ })
 
 
 
@@ -23,6 +51,7 @@ function validation(){
         errbox[0].innerHTML="Please Enter charecters only"
     }else{
         errbox[0].innerHTML=""
+        def=def+1;
     }
         
     
@@ -37,6 +66,7 @@ function emailpart(){
         errbox[1].innerHTML="Please Enter a valid email id"
     }else{
         errbox[1].innerHTML=""
+        def=def+1;
     }
 
 }
@@ -62,11 +92,12 @@ function emailpart(){
 // }
 
 function mobilepart(){
-    m=pmobile.value
+    var m=pmobile.value
     if(m.length<10 || m.length>10){
         errbox[2].innerHTML="Please Enter 10 digit mobile number"
     }else{
         errbox[2].innerHTML=""
+        def=def+1;
     }
 }
 
@@ -74,10 +105,11 @@ function mobilepart(){
         // message part  
     
 function messagepart(){
-    k=Lmesage.value
+    var k=Lmesage.value
     if(k.length<10){
         errbox[3].innerHTML="Please enter atleast 10 words"
     }else{
         errbox[3].innerHTML=""
+        def=def+1;
     }
 }
